c5dce3592611279aaa9657d588bd8d041ffb04e2
GhulamAlii <sairgust@gmail.com>
2021-07-12 18:27:09 +0500

    day 9

---
+++ Day9.py
@@ -0,0 +1,65 @@
+#################################### DIctonery  & Nesting ################################
+
+################### GRADE ASSIGN  ############
+
+#
+#
+# student_score = {
+#     "fahad":94,
+#     "aliyan":76,
+#     "usman":49,
+#     "rehman":67
+# }
+#
+#
+#
+# student_grade = {}
+#
+#
+# for key,value in student_score.items():
+#
+#     if value >90:
+#         student_grade[key] = "Outstanding"
+#     elif value >80:
+#         student_grade[key] = "great effort"
+#     elif value > 70:
+#         student_grade[key] = "Acceptable"
+#     elif value < 70:
+#         student_grade[key] = "fail"
+#
+# print(student_grade)
+
+
+################################# TRAVEL LOGGING  ################
+#
+# travel_log = [
+# {
+#   "country": "France",
+#   "visits": 12,
+#   "cities": ["Paris", "Lille", "Dijon"]
+# },
+# {
+#   "country": "Germany",
+#   "visits": 5,
+#   "cities": ["Berlin", "Hamburg", "Stuttgart"]
+# },
+# ]
+# #🚨 Do NOT change the code above
+#
+# #TODO: Write the function that will allow new countries
+# #to be added to the travel_log. 👇
+#
+# def add_new_country(country,no_city, city_visited):
+#     travel_log.append( {
+#         "country": country,
+#         "visits": no_city,
+#         "cities": city_visited
+#     })
+#
+#
+#
+# #🚨 Do not change the code below
+# add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+# print(travel_log)
+#
+#
